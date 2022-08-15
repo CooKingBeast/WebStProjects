923e353c70f1598b13d6f30755fd07139dffaf6e
CooKingBeast <f62810@yahoo.com>
2022-08-15 19:13:37 +0500

    feat: add some JS

--- NewYearTimer/script.js
+++ NewYearTimer/script.js
@@ -1,1 +1,10 @@
-console.log('123')
+const daysSelector = document.querySelector('.timer_days')
+const hoursSelector = document.querySelector('.timer_hours')
+const minutesSelector = document.querySelector('.timer_minutes')
+const secondsSelector = document.querySelector('.timer_seconds')
+
+function changeHours() {
+	console.log(hoursSelector)
+	hoursSelector.innerHTML = 'dick'
+}
+changeHours()
