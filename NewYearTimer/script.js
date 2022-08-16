74a2e58d6b31ce265ddb9649b236a8a5e422d54b
CooKingBeast <f62810@yahoo.com>
2022-08-16 13:57:07 +0500

    feat: add coundowntimer function

--- NewYearTimer/script.js
+++ NewYearTimer/script.js
@@ -3,8 +3,25 @@ const hoursSelector = document.querySelector('.timer_hours')
 const minutesSelector = document.querySelector('.timer_minutes')
 const secondsSelector = document.querySelector('.timer_seconds')
 
-function changeHours() {
-	console.log(hoursSelector)
-	hoursSelector.innerHTML = 'dick'
+function countDownTimer() {
+	const currentDate = new Date()
+	console.log(currentDate)
+	const countDownDate = new Date(2023, 0, 1)
+	console.log(countDownDate)
+	const timeToYearEnd = countDownDate - currentDate
+	const secToYearEnd = Math.floor((timeToYearEnd % (1000 * 60)) / 1000)
+	const minToYearEnd = Math.floor(
+		(timeToYearEnd % (1000 * 60 * 60)) / (1000 * 60)
+	)
+	const hoursToYearEnd = Math.floor(
+		(timeToYearEnd % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+	)
+	const daysToYearEnd = Math.floor(timeToYearEnd / (1000 * 60 * 60 * 24))
+	console.log(daysToYearEnd, hoursToYearEnd, minToYearEnd, secToYearEnd)
+
+	secondsSelector.innerHTML = secToYearEnd.toString()
+	minutesSelector.innerHTML = minToYearEnd.toString()
+	hoursSelector.innerHTML = hoursToYearEnd.toString()
+	daysSelector.innerHTML = daysToYearEnd.toString()
 }
-changeHours()
+countDownTimer()
