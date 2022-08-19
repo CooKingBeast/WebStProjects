fe19b1e97986349bbb2055a32049b26fc81ef1a9
CooKingBeast <f62810@yahoo.com>
2022-08-19 14:37:40 +0500

    feat: logic js 2 task

--- Quiz/script.js
+++ Quiz/script.js
@@ -4,6 +4,8 @@ const secondAnswerSelector = document.querySelector('.second_answer')
 const thirdAnswerSelector = document.querySelector('.third_answer')
 const fourthAnswerSelector = document.querySelector('.fourth_answer')
 const nextQueBtnSelector = document.querySelector('.next_question-button')
+const mainSelector = document.querySelector('.main')
+const allCheckboxSelector = document.querySelectorAll('.option')
 
 const data = [
 	{
@@ -21,8 +23,20 @@ const data = [
 		answer2: 'ne norm',
 		answer3: 'zaebis',
 		answer4: 'dick'
+	},
+	{
+		id: 3,
+		question: 'kak zovut?',
+		answer1: 'Petya',
+		answer2: 'Dima',
+		answer3: 'Vasya',
+		answer4: 'Dick'
 	}
 ]
+
+let number = 0
+let answer = ''
+const answersArray = []
 
 function init() {
 	questionSelector.innerText = data[0].question
@@ -33,13 +47,27 @@ function init() {
 }
 
 function nextQueBtn() {
-	questionSelector.innerText = data[1].question
-	firstAnswerSelector.innerText = data[1].answer1
-	secondAnswerSelector.innerText = data[1].answer2
-	thirdAnswerSelector.innerText = data[1].answer3
-	fourthAnswerSelector.innerText = data[1].answer4
+	number += 1
+	questionSelector.innerText = data[number].question
+	firstAnswerSelector.innerText = data[number].answer1
+	secondAnswerSelector.innerText = data[number].answer2
+	thirdAnswerSelector.innerText = data[number].answer3
+	fourthAnswerSelector.innerText = data[number].answer4
+	answersArray.push(answer)
 }
 
+function pickAnswer(event) {
+	const target = event.target
+	for (let i = 0; i < allCheckboxSelector.length; i++) {
+		allCheckboxSelector[i].checked = false
+	}
+	if (target.tagName === 'INPUT') {
+		target.checked = true
+		answer = target.labels[0].innerText
+	}
+}
+
+mainSelector.addEventListener('click', (event) => pickAnswer(event))
 nextQueBtnSelector.addEventListener('click', nextQueBtn)
 
 init()
