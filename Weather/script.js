5c5d1897a170c435ca2969b213c54205a8999ded
CooKingBeast <f62810@yahoo.com>
2022-09-06 18:02:50 +0500

    feat: add listeners, change functions to arrow type

--- Weather/script.js
+++ Weather/script.js
@@ -1,44 +1,39 @@
 const degreesSelector = document.querySelector('.degrees')
 const citySelector = document.querySelector('.city')
 const searchIconSelector = document.querySelector('.input_search_icon')
-const inputSelector = document.getElementById('label_form_input')
+const inputSelector = document.querySelector('.form_input')
 const pressureSelector = document.querySelector('.pressure')
 const humiditySelector = document.querySelector('.humidity')
 const windSelector = document.querySelector('.wind')
+const weatherIconSelector = document.querySelector('.weather_icon')
 
 let inputValue = 'Ekaterinburg'
 
-async function getWeather() {
-	let url = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=4ada66ddf05802af358c8b69ed55adaf`
+const getWeather = async () => {
+	const url = await fetch(
+		`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=4ada66ddf05802af358c8b69ed55adaf`
 	)
 	const data = await url.json()
 
-	degreesSelector.innerText = Math.round(data.main.temp - 273)
+	degreesSelector.innerText = Math.round(data.main.temp)
 	citySelector.innerText = data.name
-
+	weatherIconSelector.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png `
 	pressureSelector.innerText = data.main.pressure
 	humiditySelector.innerText = data.main.humidity
 	windSelector.innerText = Math.round(data.wind.speed)
 }
 
+searchIconSelector.addEventListener('click', () => {
+	inputValue = inputSelector.value
+	getWeather()
+})
 
-function getInputValue(e) {
-	if (e.key === 'Enter') {
-		console.log('enter pressed')
+inputSelector.addEventListener('keypress', (event) => {
+	if (event.key === 'Enter') {
+		event.preventDefault()
+		inputValue = inputSelector.value
+		getWeather()
 	}
-	// 	inputValue = inputSelector.value
-	// 	getWeather()
-	// } else inputValue = inputSelector.value
-	// getWeather()
-}
-
-// searchIconSelector.addEventListener('click', getInputValue)
-searchIconSelector.addEventListener('keypress', getInputValue)
-// 	if (e.key === 'Enter') {
-// 		inputValue = inputSelector.value
-// 		console.log('Enterpress',inputValue)
-// 	}
-// })
+})
 
 getWeather()
