2598eaf01f1c6a7aac365806f7a385921cbcd34f
CooKingBeast <f62810@yahoo.com>
2022-09-07 18:01:47 +0500

    refactor: refactor script functions

--- Weather/script.js
+++ Weather/script.js
@@ -7,25 +7,58 @@ const pressureSelector = document.querySelector('.pressure')
 const humiditySelector = document.querySelector('.humidity')
 const windSelector = document.querySelector('.wind')
 const weatherIconSelector = document.querySelector('.weather_icon')
+const btnSelector = document.querySelector('.btn')
 
-let inputValue = 'Ekaterinburg'
+// let inputValue = 'Ekaterinburg'
 const apiKey = '4ada66ddf05802af358c8b69ed55adaf'
-
-const getWeather = async () => {
-	const response = await fetch(
-		`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=${apiKey}`
-	)
-
-	const data = await response.json()
-}
-// const tempName = () => {
-// 	degreesSelector.innerText = Math.round(data.main.temp)
-// 	citySelector.innerText = data.name
-// 	weatherIconSelector.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png `
-// 	pressureSelector.innerText = data.main.pressure
-// 	humiditySelector.innerText = data.main.humidity
-// 	windSelector.innerText = Math.round(data.wind.speed)
+let temp = 2
+//
+// const fnc = (res) => {
+// 	temp = 4
+// 	const data = {
+// 		id: 1
+// 	}
+// 	console.log('fnc', temp)
+// 	return data//
 // }
+//
+// console.log('glob', temp)
+// const handleClick = () => {
+//
+// 	const result = fnc()
+// 	console.log('handleClick', result)
+// }
+
+const init = () => {
+	const defaultCity = 'Moscow'
+	getWeather(defaultCity).then((data) => createWeatherContent(data))
+	// getWeather(defaultCity)
+}
+
+const getWeather = async (defaultCity) => {
+	const response = await fetch(
+		`https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&units=metric&appid=${apiKey}`
+	)
+	const data = await response.json()
+
+	return data
+	// const res = {}
+	// fetch(
+	// 	`https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&units=metric&appid=${apiKey}`
+	// )
+	// 	.then((resp) => resp.json())
+	// 	.then((data) => createWeatherContent(data))
+		// .finally((data)=> createWeatherContent(data))
+}
+const createWeatherContent = (data) => {
+	console.log('data', data)
+	// degreesSelector.innerText = Math.round(data.main.temp)
+	// citySelector.innerText = data.name
+	// weatherIconSelector.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png `
+	// pressureSelector.innerText = data.main.pressure
+	// humiditySelector.innerText = data.main.humidity
+	// windSelector.innerText = Math.round(data.wind.speed)
+}
 
 searchIconSelector.addEventListener('click', () => {
 	inputValue = inputSelector.value
@@ -46,4 +79,6 @@ formSelector.addEventListener('submit', (event) => {
 	getWeather()
 })
 
-getWeather()
+btnSelector.addEventListener('click', () => {})
+
+init()
