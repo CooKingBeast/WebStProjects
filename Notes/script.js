7a11777598be73bba04f0b20efc30c09d7412498
CooKingBeast <f62810@yahoo.com>
2022-09-21 20:29:51 +0500

    feat: add func and listener for edit button

--- Notes/script.js
+++ Notes/script.js
@@ -1,12 +1,14 @@
 const controlButtonSelector = document.querySelector('.control-button')
 const contentSelector = document.querySelector('.content')
 
-let id = 0
+let deleteBtnID = 0
+let editBtnID = 1000
 
 const handleAddNote = () => {
 	const newItem = document.createElement('div')
 	newItem.className = 'item'
-	id += 1
+	deleteBtnID += 1
+	editBtnID += 1
 	newItem.insertAdjacentHTML(
 		'afterbegin',
 		`<div class="item_text_block">
@@ -15,8 +17,8 @@ const handleAddNote = () => {
 							</label>
 					</div>
 					<div class="buttons_panel">
-								<button class="edit">Edit</button>
-								<button class="delete" id="${id}">Delete</button>
+								<button class="edit" id="${editBtnID}">Edit</button>
+								<button class="delete" id="${deleteBtnID}">Delete</button>
 					</div>`
 	)
 	contentSelector.append(newItem)
@@ -26,10 +28,17 @@ const handleDeleteItem = (deleteTarget) => {
 	deleteTarget.remove()
 }
 
+const handleEditItem = () => {
+	console.log('click')
+}
+
 controlButtonSelector.addEventListener('click', handleAddNote)
+
 contentSelector.addEventListener('click', (event) => {
 	const itemPath = event.target.closest('.item')
 	if (event.target.id > 0 && event.target.id < 999) {
 		handleDeleteItem(itemPath)
+	} else if (event.target.id > 1000 && event.target.id < 1999) {
+		handleEditItem()
 	}
 })
